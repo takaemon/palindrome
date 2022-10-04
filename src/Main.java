8ab963d9d911f81b22b373bde2cb92d566e6eb04
takaemon <takaemonofficial@gmail.com>
2022-10-04 21:38:15 +0800

    string input

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(new char[]{'a','b','a','b'}));
+        System.out.println(isPalindrome("abcddcba"));
     }
 
-    static boolean isPalindrome(char[] input) {
+    static boolean isPalindrome(String str) {
         int i = 0;
-        int j = input.length - 1;
+        int j = str.length() - 1;
         while (i < j) {
-            if (input[i] != input[j]) {
+            if (str.charAt(i) != str.charAt(j)) {
                 return false;
             }
             i++;
