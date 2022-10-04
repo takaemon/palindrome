5e532ac96ee888b158985223b63bc3e1b7332dde
takaemon <takaemonofficial@gmail.com>
2022-10-04 21:36:56 +0800

    char array input

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(new char[]{'a','b','a'}));
+        System.out.println(isPalindrome(new char[]{'a','b','a','b'}));
     }
 
     static boolean isPalindrome(char[] input) {
-        String str = new String(input);
         int i = 0;
-        int j = str.length() - 1;
+        int j = input.length - 1;
         while (i < j) {
-            if (str.charAt(i) != str.charAt(j)) {
+            if (input[i] != input[j]) {
                 return false;
             }
             i++;
